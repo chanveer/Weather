923807bc6a372fdf2acf0ed881aaea45acb0a389
chanveer <chm026@gmail.com>
2018-04-10 12:35:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
-
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const restService = express();
+var dateFormat = require('dateformat');
+var http = require('https');
+//var os = require('os');
 
 restService.use(
   bodyParser.urlencoded({
@@ -13,35 +14,135 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-  var jsonObj = require("./db.json");
-  var string1 = "";
-  for (var property1 in jsonObj.employess) {
-	  string1 = string1 + jsonObj.employess[property1].name + " : " + jsonObj.employess[property1].shift + " ";
-  }
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: string1,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
+restService.post('/echo', function(req, res) {
+    console.log('=============' + req.body.result.action)
+    switch (req.body.result.action) {
+		 case "callSchedule":
+			callApi1data().then((output) => {
+				var string2 = "";
+				for(var property1 in output) {
+					for(var property2 in output[property1].schedule) {
+						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
+						if(dateexcel   == req.body.result.parameters.date){
+							string2 =   string2 + output[property1].firstname + "  "  +  output[property1].schedule[property2].starttime  + " : " + output[property1].schedule[property2].endtime +  ' ';
+						}
+					}	
 
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
+				}
+                return res.json({
+                    speech: string2 + " are on schedule.",
+                    source: 'webhook-echo-one',
+         
+                });
+			});
+		break;
+
+        case "callleave":
+            callApi1data().then((output) => {
+				var string3 = "";
+				for(var property1 in output) {
+					for(var property2 in output[property1].schedule) {
+						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
+						if((output[property1].schedule[property2].status   == 0)&&(dateexcel   == req.body.result.parameters.date1)){
+							string3 =   string3 + output[property1].firstname + "  "  +  output[property1].schedule[property2].date + " " + output[property1].schedule[property2].status    + ' ';
+						}
+					}	
+				}
+				return res.json({
+                    speech: string3 + " are on leave.",
+                    source: 'webhook-echo-one'
+                });
+			});
+        break;
+
+			
+		case "callAvail":
+            callApi1data().then((output) => {
+				
+				var string1 = "";
+				for(var property1 in output) {
+					for(var property2 in output[property1].schedule) {
+						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
+							if((output[property1].schedule[property2].status   == 1)&&(dateexcel   == req.body.result.parameters.date2)){
+							string1 =   string1 + output[property1].firstname + "  "  +  output[property1].schedule[property2].date + " " + output[property1].schedule[property2].status    + ' ';
+							}
+					}	
+
+				}
+				
+				return res.json({
+                    speech: string1 + " are available.",
+                    source: 'webhook-echo-one'
+                });
+			});
+         break;
     }
-  });
 });
 
+	
+function callApi1data () {
+  return new Promise((resolve, reject) => {
+	var GoogleSpreadsheet = require('google-spreadsheet');
+	var creds = require('./client_secret.json');
+	// Create a document object using the ID of the spreadsheet - obtained from its URL.
+	var doc = new GoogleSpreadsheet('1sMkMyVP9eRXQ6HmU9BTXuDJP5WPC28RdslbCPl_fR9Q');
+
+	// Authenticate with the Google Spreadsheets API.
+	doc.useServiceAccountAuth(creds, function (err) {
+                                
+		// Get all of the rows from the spreadsheet.
+		if(err){
+		 reject(err);	
+		}else{
+			// Get all of the rows from the spreadsheet.
+			doc.getRows(1, function (err1, rows1) {
+				if (err1) {
+					console.log(err1);
+					reject(err1);
+				}
+				else {
+					var data1 = rows1;
+					doc.getRows(2, function (err2, rows2) {
+						if (err2) {
+							console.log(err2);
+							reject(err2);
+						}
+						else {
+							var data = [];
+							var data2 = rows2;
+							for (let row1 in data1) {
+								var emp = {};
+								emp.empid = data1[row1]['empid'];
+								emp.firstname = data1[row1]['firstname'];
+								emp.lastname = data1[row1]['lastname'];
+								emp.email = data1[row1]['email'];
+								emp.phone = data1[row1]['phone'];
+								if (!('schedule' in emp))
+												emp.schedule = [];
+								for (let row2 in data2) {
+									var sch = {};
+									if (data2[row2]['employeeid'] == data1[row1]['empid']) {
+										sch.date = data2[row2]['date'];
+										sch.employeeid = data2[row2]['employeeid'];
+										sch.starttime = data2[row2]['starttime'];
+										sch.endtime = data2[row2]['endtime'];
+										sch.location = data2[row2]['location'];
+										sch.status = data2[row2]['status'];
+										emp.schedule.push(sch);
+									}
+								}
+								data.push(emp);
+							}
+							resolve(data);
+						}
+					});
+				}
+			});			
+		}
+	});
+  })
+}
+ 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
