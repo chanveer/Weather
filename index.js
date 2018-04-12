60e207d60a874336b2a39f6a599aea886a1691b4
chanveer <chm026@gmail.com>
2018-04-12 12:52:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 "use strict";
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const restService = express();
-var dateFormat = require('dateformat');
-var http = require('https');
-//var os = require('os');
 
 restService.use(
   bodyParser.urlencoded({
@@ -14,135 +13,188 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
-    console.log('=============' + req.body.result.action)
-    switch (req.body.result.action) {
-		 case "callSchedule":
-			callApi1data().then((output) => {
-				var string2 = "";
-				for(var property1 in output) {
-					for(var property2 in output[property1].schedule) {
-						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
-						if(dateexcel   == req.body.result.parameters.date){
-							string2 =   string2 + output[property1].firstname + " from "  +  output[property1].schedule[property2].starttime  + " to " + output[property1].schedule[property2].endtime + " @ " + output[property1].schedule[property2].location +  ' ; ';
-						}
-					}	
-
-				}
-                return res.json({
-                    speech: " Sure. let me check and provide the detail; " +string2,
-                    source: 'webhook-echo-one',
-         
-                });
-			});
-		break;
-
-        case "callleave":
-            callApi1data().then((output) => {
-				var string3 = "";
-				for(var property1 in output) {
-					for(var property2 in output[property1].schedule) {
-						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
-						if((output[property1].schedule[property2].status   == 0)&&(dateexcel   == req.body.result.parameters.date1)){
-							string3 =   string3 + output[property1].firstname + " from  "  +  output[property1].schedule[property2].starttime + " to " + output[property1].schedule[property2].endtime + " @ " + output[property1].schedule[property2].location   + ' ; ';
-						}
-					}	
-				}
-				return res.json({
-                    speech: " Sure. Let me provide the list .  "+string3,
-                    source: 'webhook-echo-one'
-                });
-			});
-        break;
-
-			
-		case "callAvail":
-            callApi1data().then((output) => {
-				
-				var string1 = "";
-				for(var property1 in output) {
-					for(var property2 in output[property1].schedule) {
-						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
-							if((output[property1].schedule[property2].status   == 1)&&(dateexcel   == req.body.result.parameters.date2)){
-							string1 =   string1 + output[property1].firstname + " from "  +  output[property1].schedule[property2].starttime + " to " + output[property1].schedule[property2].endtime  + " @ " + output[property1].schedule[property2].location  +  ' ; ';
-							}
-					}	
-
-				}
-				
-				return res.json({
-                    speech: "Yeah. I see few subs are available for the work;  "+string1,
-                    source: 'webhook-echo-one'
-                });
-			});
-         break;
-    }
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
 
-	
-function callApi1data () {
-  return new Promise((resolve, reject) => {
-	var GoogleSpreadsheet = require('google-spreadsheet');
-	var creds = require('./client_secret.json');
-	// Create a document object using the ID of the spreadsheet - obtained from its URL.
-	var doc = new GoogleSpreadsheet('1sMkMyVP9eRXQ6HmU9BTXuDJP5WPC28RdslbCPl_fR9Q');
+restService.post("/audio", function(req, res) {
+  var speech = "";
+  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+    //Speech Synthesis Markup Language 
+    case "music one":
+      speech =
+        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music two":
+      speech =
+        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music three":
+      speech =
+        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music four":
+      speech =
+        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music five":
+      speech =
+        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
+      break;
+    case "delay":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
+    case "cardinal":
+      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
+      break;
+    case "ordinal":
+      speech =
+        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
+      break;
+    case "characters":
+      speech =
+        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
+      break;
+    case "fraction":
+      speech =
+        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
+      break;
+    case "bleep":
+      speech =
+        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
+      break;
+    case "unit":
+      speech =
+        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
+      break;
+    case "verbatim":
+      speech =
+        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
+      break;
+    case "date one":
+      speech =
+        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
+      break;
+    case "date two":
+      speech =
+        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
+      break;
+    case "date three":
+      speech =
+        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
+      break;
+    case "time":
+      speech =
+        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
+      break;
+    case "telephone one":
+      speech =
+        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
+      break;
+    case "telephone two":
+      speech =
+        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
+      break;
+    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
+    case "alternate":
+      speech =
+        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
+      break;
+  }
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
 
-	// Authenticate with the Google Spreadsheets API.
-	doc.useServiceAccountAuth(creds, function (err) {
-                                
-		// Get all of the rows from the spreadsheet.
-		if(err){
-		 reject(err);	
-		}else{
-			// Get all of the rows from the spreadsheet.
-			doc.getRows(1, function (err1, rows1) {
-				if (err1) {
-					console.log(err1);
-					reject(err1);
-				}
-				else {
-					var data1 = rows1;
-					doc.getRows(2, function (err2, rows2) {
-						if (err2) {
-							console.log(err2);
-							reject(err2);
-						}
-						else {
-							var data = [];
-							var data2 = rows2;
-							for (let row1 in data1) {
-								var emp = {};
-								emp.empid = data1[row1]['empid'];
-								emp.firstname = data1[row1]['firstname'];
-								emp.lastname = data1[row1]['lastname'];
-								emp.email = data1[row1]['email'];
-								emp.phone = data1[row1]['phone'];
-								if (!('schedule' in emp))
-												emp.schedule = [];
-								for (let row2 in data2) {
-									var sch = {};
-									if (data2[row2]['employeeid'] == data1[row1]['empid']) {
-										sch.date = data2[row2]['date'];
-										sch.employeeid = data2[row2]['employeeid'];
-										sch.starttime = data2[row2]['starttime'];
-										sch.endtime = data2[row2]['endtime'];
-										sch.location = data2[row2]['location'];
-										sch.status = data2[row2]['status'];
-										emp.schedule.push(sch);
-									}
-								}
-								data.push(emp);
-							}
-							resolve(data);
-						}
-					});
-				}
-			});			
-		}
-	});
-  })
-}
- 
+restService.post("/video", function(req, res) {
+  return res.json({
+    speech:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    displayText:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/slack-test", function(req, res) {
+  var slack_message = {
+    text: "Details of JIRA board for Browse and Commerce",
+    attachments: [
+      {
+        title: "JIRA Board",
+        title_link: "http://www.google.com",
+        color: "#36a64f",
+
+        fields: [
+          {
+            title: "Epic Count",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Story Count",
+            value: "40",
+            short: "false"
+          }
+        ],
+
+        thumb_url:
+          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+      },
+      {
+        title: "Story status count",
+        title_link: "http://www.google.com",
+        color: "#f49e42",
+
+        fields: [
+          {
+            title: "Not started",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          }
+        ]
+      }
+    ]
+  };
+  return res.json({
+    speech: "speech",
+    displayText: "speech",
+    source: "webhook-echo-sample",
+    data: {
+      slack: slack_message
+    }
+  });
+});
+
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
