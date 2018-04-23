c7fd2a8dc14305037c9597ebbe61817795f2154b
chanveer <chm026@gmail.com>
2018-04-23 12:34:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ restService.post('/echo', function(req, res) {
 							
 															
 							if(req.body.result.parameters.location == ''){
-								string2 =   "with date";
+								//string2 =   "with date";
 								string2 =   string2 + output[property1].firstname + " from "  +  output[property1].schedule[property2].starttime  + " to " + output[property1].schedule[property2].endtime + " @ " + output[property1].schedule[property2].location +  ' ; ';
 								cntavail++;
 							}
