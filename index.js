c640d071886a7d851f7947a63fcb9ad60206a0b0
chanveer <chm026@gmail.com>
2018-04-23 12:47:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ restService.post('/echo', function(req, res) {
 							
 							
 															
-							if( (req.body.result.parameters.location != '') && ( output[property1].schedule[property2].location == req.body.result.parameters.location.city)){
+							if( (req.body.result.parameters.any != '') && ( output[property1].schedule[property2].location == req.body.result.parameters.any)){
 									//string2 =   "with city and date";
 									string2 =   string2 + output[property1].firstname + " from "  +  output[property1].schedule[property2].starttime  + " to " + output[property1].schedule[property2].endtime + " @ " + output[property1].schedule[property2].location +  ' ; ';
 									cntavail++;
