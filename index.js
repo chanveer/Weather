69436a0492646b3ab82df628844a0a0b2edbad13
chanveer <chm026@gmail.com>
2018-04-24 12:03:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ restService.post('/echo', function(req, res) {
 							}
 							
 							cnt++;
-							var givendate = dateFormat(req.body.result.parameters.date, "yyyy-mm-dd");	
+							var givendate = dateFormat(req.body.result.parameters.date, "dd-mmm-yyyy");	
 							var result = "";
 							if(cntleave != 0){       // including present as well as absent;
-								result = "Sure.Total of "+cnt+" employess on schedule for "+givendate+" at . However "+cntleave+" have reported that the would not be able to make their shift.";
+								result = "Sure.Total of "+cnt+" employess on schedule for "+givendate+" . However "+cntleave+" have reported that the would not be able to make their shift.";
 							}else{
 								var result = "Sure.Total of "+cnt+" employess on schedule for "+givendate+". "+string2;
 							}
